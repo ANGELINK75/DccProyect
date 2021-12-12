aeb74b660f88b7b12a50b56cc67882534dd7b187
AngeLink <tiranosaurio-r-ex@hotmail.com>
2021-12-11 22:39:34 -0600

    Final Commit

--- mayapy/cube.py
+++ mayapy/cube.py
@@ -4,7 +4,7 @@ import maya.cmds as cmds
 standalone.initialize(name='python') 
 cmds.file(f=True, new=True)
 
-cmds.polyCube(name='UP_Cube-Dado')
+cmds.polyCube(name='UP_Cube-c')
 
-cmds.file(rename='UP_Cube-Dado.ma')
+cmds.file(rename='UP_Cube-c.ma')
 cmds.file(force=True, type='mayaAscii', save=True)
--- mayapy/sphere.py
+++ mayapy/sphere.py
@@ -4,7 +4,7 @@ import maya.cmds as cmds
 standalone.initialize(name='python') 
 cmds.file(f=True, new=True)
 
-cmds.sphere(r=2, name='UP_Sphere-Bola')
+cmds.sphere(r=2, name='UP_Sphere-s')
 
-cmds.file(rename='UP_Sphere-Bola.ma')
+cmds.file(rename='UP_Sphere-s.ma')
 cmds.file(force=True, type='mayaAscii', save=True)
